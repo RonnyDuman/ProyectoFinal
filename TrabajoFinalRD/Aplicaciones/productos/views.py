d7158e881f3f98f16ea993e94d9343bee78af336
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-06 01:27:20 -0500

    Renderizamos lo que es (nuevo_producto)

--- TrabajoFinalRD/Aplicaciones/productos/views.py
+++ TrabajoFinalRD/Aplicaciones/productos/views.py
@@ -14,3 +14,17 @@ def nuevo_producto(request):
         stock = request.POST.get('stock')
         categoria_id = request.POST.get('categoria')
         imagen = request.FILES.get('imagen')
+
+        if all([nombre, descripcion, precio, stock, categoria_id]):
+            Producto.objects.create(
+                nombre=nombre,
+                descripcion=descripcion,
+                precio=precio,
+                stock=stock,
+                imagen=imagen,
+                categoria_id=categoria_id
+            )
+            return redirect('General')
+
+    return render(request, 'productos/nuevo.html', {'categorias': categorias})
+
