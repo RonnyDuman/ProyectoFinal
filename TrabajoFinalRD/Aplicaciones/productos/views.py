eeb93e46bbb62d712aba5d3d0b09ee4a0b56f722
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-06 01:26:03 -0500

    Definimos la vista para productos (nuevo_producto)

--- TrabajoFinalRD/Aplicaciones/productos/views.py
+++ TrabajoFinalRD/Aplicaciones/productos/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from Aplicaciones.productos.models import Producto
+from Aplicaciones.categorias.models import Categoria
 
 # Create your views here.
+
+def nuevo_producto(request):
+    categorias = Categoria.objects.all()
+
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        descripcion = request.POST.get('descripcion')
+        precio = request.POST.get('precio').replace(",",".")
+        stock = request.POST.get('stock')
+        categoria_id = request.POST.get('categoria')
+        imagen = request.FILES.get('imagen')
