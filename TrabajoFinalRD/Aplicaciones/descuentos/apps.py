fdd11bc8d968e0efdb351f10d7631bf67a1baf7b
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-06 14:36:51 -0500

    Agregamos a settings nuestra app

--- TrabajoFinalRD/Aplicaciones/descuentos/apps.py
+++ TrabajoFinalRD/Aplicaciones/descuentos/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DescuentosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'descuentos'
+    name = 'Aplicaciones.descuentos'
