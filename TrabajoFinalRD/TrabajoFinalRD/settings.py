75011f47f4b87797c0d10f6d490fdb6fd2e72a8e
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-06 01:46:36 -0500

    Creamos la aplicacion carrito

--- TrabajoFinalRD/TrabajoFinalRD/settings.py
+++ TrabajoFinalRD/TrabajoFinalRD/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Aplicaciones.usuarios',
     'Aplicaciones.categorias',
-    'Aplicaciones.productos'
+    'Aplicaciones.productos',
+    'Aplicaciones.carrito',
+    
+
 
 ]
 
