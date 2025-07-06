ee3f809c3926138e2c65ad264e474fd0c1ff4a69
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-06 00:32:36 -0500

    Creamos las vista para usuarios (creamos la clase registro)

--- TrabajoFinalRD/Aplicaciones/usuarios/models.py
+++ TrabajoFinalRD/Aplicaciones/usuarios/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Usuario(models.Model):
     nombre = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
-    contraseña = models.CharField(max_length=128)  # Hasheada en la lógica
+    contraseña = models.CharField(max_length=128)
     direccion = models.TextField(blank=True, null=True)
     telefono = models.CharField(max_length=20, blank=True, null=True)
     
