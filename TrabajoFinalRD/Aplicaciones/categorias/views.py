1e471b860c726bc9dfb5139482a750fd3a4c636e
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-07 20:03:20 -0500

    Realizamos importaciones para la vista de categorias

--- TrabajoFinalRD/Aplicaciones/categorias/views.py
+++ TrabajoFinalRD/Aplicaciones/categorias/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
+from django.db.models import ProtectedError
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponseNotAllowed
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseNotAllowed, JsonResponse
+from django.views.decorators.http import require_http_methods
+from Aplicaciones.categorias.models import Categoria
 
 # Create your views here.
