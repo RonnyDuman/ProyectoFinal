16434f2a86dab28b1d70dd7eb29b3895e962fe21
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-16 23:36:46 -0500

    Agregamos las nuevas rutas

--- Aplicaciones/productos/urls.py
+++ Aplicaciones/productos/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('nuevo/', views.nuevo_producto, name='nuevo_producto'),
 
 
+    path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
+    path('productoEd/<int:producto_id>/', views.detalle_productoEd, name='detalle_productoEd'),
+    path('producto/eliminar/<int:producto_id>/', views.eliminar_producto, name='eliminar_producto'),
+
+
+
 ]
