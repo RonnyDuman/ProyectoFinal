859bed1602831bbe1c550dd4da52c9ab1b6c0531
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-19 04:00:14 -0500

    definimos la funcion llamada carrito_context

--- Aplicaciones/carrito/context_processors.py
+++ Aplicaciones/carrito/context_processors.py
@@ -1,3 +1,6 @@
 from django.db.models import Sum
 from Aplicaciones.carritoproductos.models import CarritoProducto
 from Aplicaciones.carrito.models import Carrito
+
+#Definimos la funcion 
+def carrito_context(request):
