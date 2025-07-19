bb2703a497a1978963521b30cc1e3ef965844a8d
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-18 22:27:26 -0500

    Verificamos si tiene descuento el producto

--- Aplicaciones/carrito/views.py
+++ Aplicaciones/carrito/views.py
@@ -5,3 +5,11 @@ from Aplicaciones.usuarios.models import Usuario
 from Aplicaciones.carritoproductos.models import CarritoProducto
 from decimal import Decimal
 from django.shortcuts import render, get_object_or_404, redirect
+
+#Definimos nuestra funcion
+def agregar_al_carrito(request, id):
+    producto = get_object_or_404(Producto, id=id)
+
+    #Verificar si tiene descuento
+    descuento_obj = Descuento.objects.filter(producto=producto).first()
+    porcentaje_descuento = descuento_obj.porcentaje_descuento if descuento_obj else Decimal('0')
