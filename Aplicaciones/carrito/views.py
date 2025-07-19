dde538e6820b5ab4de2d3dbf411976a60bf51511
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-18 22:22:51 -0500

    Configuramos nuestra vista de app carrito y agregamos las importaciones

--- Aplicaciones/carrito/views.py
+++ Aplicaciones/carrito/views.py
@@ -1,1 +1,7 @@
-
+from Aplicaciones.productos.models import Producto
+from Aplicaciones.descuentos.models import Descuento
+from Aplicaciones.carrito.models import Carrito
+from Aplicaciones.usuarios.models import Usuario
+from Aplicaciones.carritoproductos.models import CarritoProducto
+from decimal import Decimal
+from django.shortcuts import render, get_object_or_404, redirect
