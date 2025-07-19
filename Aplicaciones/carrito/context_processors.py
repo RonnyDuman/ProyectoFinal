c9d0048271fed14ad8f50e50a83edd0be47f0036
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-19 04:10:15 -0500

    Obtiene el carrito guardado en la sesión del usuario

--- Aplicaciones/carrito/context_processors.py
+++ Aplicaciones/carrito/context_processors.py
@@ -4,3 +4,7 @@ from Aplicaciones.carrito.models import Carrito
 
 #Definimos la funcion 
 def carrito_context(request):
+    #Obtiene el carrito guardado en la sesión del usuario y suma todas las cantidades de productos en el carrito.
+    carrito = request.session.get('carrito', {})
+    total_items = sum(item['cantidad'] for item in carrito.values())
+    return {'total_items_carrito': total_items}
