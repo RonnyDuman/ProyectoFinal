731c7d194ebcee13be0ded6123aa353f310c5bd2
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-19 04:12:22 -0500

    Definnimos la funcion llamada carrito_total_items

--- Aplicaciones/carrito/context_processors.py
+++ Aplicaciones/carrito/context_processors.py
@@ -8,3 +8,6 @@ def carrito_context(request):
     carrito = request.session.get('carrito', {})
     total_items = sum(item['cantidad'] for item in carrito.values())
     return {'total_items_carrito': total_items}
+
+#Definimos la funcion 
+def carrito_total_items(request):
