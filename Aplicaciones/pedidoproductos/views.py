0000111b7cc4a2570882b1bd13a0ddb6942672c9
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-19 05:04:34 -0500

    Ahora vamos a configurar lo que es la app pedidoproducto y añadimos las importaciones

--- Aplicaciones/pedidoproductos/views.py
+++ Aplicaciones/pedidoproductos/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
+from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from Aplicaciones.pedidos.models import Pedido
+from Aplicaciones.pedidoproductos.models import PedidoProducto
+from Aplicaciones.pagos.models import Pago
+from Aplicaciones.productos.models import Producto
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from Aplicaciones.carrito.models import Carrito
+from Aplicaciones.productos.models import Producto
+from Aplicaciones.usuarios.models import Usuario
 # Create your views here.
