85acac4c57a5321a80a71aa3872f61427734a346
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-19 05:54:26 -0500

    ahora vamos a configurar la vista de la app pagos e importamos

--- Aplicaciones/pagos/views.py
+++ Aplicaciones/pagos/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from Aplicaciones.pedidos.models import Pedido
+from Aplicaciones.pedidoproductos.models import PedidoProducto
+from Aplicaciones.pagos.models import Pago
+from Aplicaciones.productos.models import Producto
 
 # Create your views here.
