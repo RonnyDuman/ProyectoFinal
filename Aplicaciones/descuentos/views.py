f96b236137ca24d1b96cae3234de7fce66c8df95
RonnyHr <ronny.dumancela0139@utc.edu.ec>
2025-07-18 21:45:59 -0500

    Ahora vamos a configurar lo que es la app descuentos e importaremos los modelos Producto y Descuento

--- Aplicaciones/descuentos/views.py
+++ Aplicaciones/descuentos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from Aplicaciones.productos.models import Producto
+from Aplicaciones.descuentos.models import Descuento
+from django.utils import timezone
 
 # Create your views here.
+
+
